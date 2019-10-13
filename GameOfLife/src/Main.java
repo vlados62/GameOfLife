0fbce55ccdf06336e8c0d580909fb401fadc4d6b
Vladislav Anashkin <vladis62@yandex.ru>
2019-10-13 13:45:52 +0300

    "ver 1.2"

--- GameOfLife/src/Main.java
+++ GameOfLife/src/Main.java
@@ -1,22 +1,46 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws IOException {
+
         String[][] square = initialSquare();
         printSquare(square);
-       /* Thread.sleep(1000);
-
-        square = initialSquare1();
-        printSquare(square);
-        Thread.sleep(1000);
-        square = initialSquare2();
-        printSquare(square);
-        Thread.sleep(1000);
-        Runtime.getRuntime().exec("clear");*/
+        String coordinate = getCoordinate();
+        System.out.println(coordinate);
 
     }
 
+    public static String getCoordinate() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str, pattern = null;
+        while (true) {
+            System.out.println("Введите клетку: (Пример ввода: A4 или H10)");
+            str = reader.readLine();
+            if (str.length() == 2) {
+                pattern = "([A-J][1-9])";
+            } else if (str.length() == 3) {
+                pattern = "([A-J]10)";
+            }
+            if (pattern != null) {
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(str);
+                if (m.find()) {
+                    break;
+                } else {
+                    System.out.println("Некорректный ввод.");
+                }
+            }
+            System.out.println("Некорректный ввод.");
+        }
+        reader.close();
+        return str;
+    }
 
     public static String[][] initialSquare() {
         String[][] str = new String[10][10];
@@ -31,12 +55,12 @@ public class Main {
     public static void printSquare(String[][] str) {
         System.out.println("   A B C D E F G H I J");
         for (int i = 0; i < 10; i++) {
-            if (i != 9) System.out.print((i + 1) + "  "); else System.out.print((i + 1) + " ");
+            if (i != 9) System.out.print((i + 1) + "  ");
+            else System.out.print((i + 1) + " ");
             for (int j = 0; j < 10; j++) {
                 System.out.print(str[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
     }
 }
